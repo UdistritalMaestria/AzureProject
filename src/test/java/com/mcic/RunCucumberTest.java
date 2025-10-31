8623ee378e145f15fb731e72794ed56e6780e0b9
andresitogutu04 <pipelindo0278@hotmail.com>
2025-10-31 09:04:32 -0500

    Fix resource path from 'feature' to 'features'

--- src/test/java/com/mcic/RunCucumberTest.java
+++ src/test/java/com/mcic/RunCucumberTest.java
@@ -5,7 +5,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("feature") // ❌ Error: debería ser "features"
+@SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.mcic.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
         value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
